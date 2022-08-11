ebed087d99fc64f5b4d88f10eca199cdece5e374
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-08-11 20:34:30 +0430

    Create Basket.API Project,Controller,Entities and Repositories

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItems> Items { get; set; } = new List<ShoppingCartItems>();
+
+        public ShoppingCart()
+        {
+        }
+
+        public ShoppingCart(string username)
+        {
+            this.UserName = username;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                decimal totalprice = 0;
+                foreach (var item in Items)
+                {
+                    totalprice += item.Price * item.Quantity;
+                }
+                return totalprice;
+            }
+        }
+    }
+}
