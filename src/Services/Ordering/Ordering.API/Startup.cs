5820f2f1864cf37bc8e05539ee5394b6f4b4d595
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-08-15 13:22:07 +0430

    Register Services

--- src/Services/Ordering/Ordering.API/Startup.cs
+++ src/Services/Ordering/Ordering.API/Startup.cs
@@ -1,15 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Ordering.Application;
 
 namespace Ordering.API
 {
@@ -25,6 +20,8 @@ namespace Ordering.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddApplicationServices();
+            //services.AddInfrastructureServices();
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
