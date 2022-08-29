d2bf69715c60303d2aad15f6abec06c8952b7236
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-08-29 18:07:11 +0430

    Adding SeriLog for OcelotApiGw Microservices for Logging on ElasticSearch

--- src/ApiGateways/OcelotApiGw/Program.cs
+++ src/ApiGateways/OcelotApiGw/Program.cs
@@ -1,7 +1,9 @@
+using Common.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace OcelotApiGw
 {
@@ -22,11 +24,12 @@ namespace OcelotApiGw
                 {
                     webBuilder.UseStartup<Startup>();
                 })
-                .ConfigureLogging((hostingContext, loggingbuilder) =>
-                {
-                    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
-                    loggingbuilder.AddConsole();
-                    loggingbuilder.AddDebug();
-                });
+                .UseSerilog(SeriLogger.Configure);
+        //.ConfigureLogging((hostingContext, loggingbuilder) =>
+        //{
+        //    loggingbuilder.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+        //    loggingbuilder.AddConsole();
+        //    loggingbuilder.AddDebug();
+        //});
     }
 }
