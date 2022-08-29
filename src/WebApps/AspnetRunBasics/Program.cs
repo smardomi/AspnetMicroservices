8ca9b3e043edfe5c503aae0f682cc9e46b839c90
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-08-29 17:48:38 +0430

    Adding SeriLog Common Logging Library Project References and Adding LoggingDelegatingHandler for Intercepting Microservices Request-Response

--- src/WebApps/AspnetRunBasics/Program.cs
+++ src/WebApps/AspnetRunBasics/Program.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Reflection;
+using Common.Logging;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -17,27 +18,7 @@ namespace AspnetRunBasics
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseSerilog((context, configuration) =>
-                {
-                    var elasticUri = context.Configuration.GetValue<string>("ElasticConfiguration:Uri");
-
-                    configuration
-                        .Enrich.FromLogContext()
-                        .Enrich.WithMachineName()
-                        .WriteTo.Debug()
-                        .WriteTo.Console()
-                        .WriteTo.Elasticsearch(
-                            new ElasticsearchSinkOptions(new Uri(elasticUri))
-                            {
-                                IndexFormat = $"applogs-{Assembly.GetExecutingAssembly().GetName().Name.ToLower().Replace(".", "-")}-{context.HostingEnvironment.EnvironmentName?.ToLower().Replace(".", "-")}-{DateTime.UtcNow:yyyy-MM}",
-                                AutoRegisterTemplate = true,
-                                NumberOfShards = 2,
-                                NumberOfReplicas = 1
-                            })
-                        .Enrich.WithProperty("Environment", context.HostingEnvironment.EnvironmentName)
-                        //.Enrich.WithProperty("Application", context.HostingEnvironment.ApplicationName)
-                        .ReadFrom.Configuration(context.Configuration);
-                })
+                .UseSerilog(SeriLogger.Configure)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
