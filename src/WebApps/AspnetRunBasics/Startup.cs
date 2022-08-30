019e388e0e4b2202f860b021ac331faf7f13645b
Saeed Mardomi <saeedmardomi@hotmail.com>
2022-08-30 11:52:55 +0430

    Adding Health Check for AspnetRunBasics Shopping Microservices with OcelotGw

--- src/WebApps/AspnetRunBasics/Startup.cs
+++ src/WebApps/AspnetRunBasics/Startup.cs
@@ -3,10 +3,13 @@ using System.Net.Http;
 using AspnetRunBasics.Services.Contracts;
 using AspnetRunBasics.Services.Implementations;
 using Common.Logging;
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using Polly;
 using Polly.Extensions.Http;
@@ -49,6 +52,9 @@ namespace AspnetRunBasics
                 .AddPolicyHandler(GetCircuitBreakerPolicy()); ;
 
             services.AddRazorPages();
+
+            services.AddHealthChecks()
+                .AddUrlGroup(new Uri(Configuration["ApiSettings:GatewayAddress"]), "Ocelot API Gw", HealthStatus.Degraded);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -75,6 +81,11 @@ namespace AspnetRunBasics
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
+                endpoints.MapHealthChecks("/hc", new HealthCheckOptions()
+                {
+                    Predicate = _ => true,
+                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+                });
             });
         }
 
